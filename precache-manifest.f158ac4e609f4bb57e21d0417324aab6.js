64b8ab868c256457b85b14d9ad7869442219c33e
dsaltares <dsaltares@users.noreply.github.com>
2020-04-21 13:44:28 +0000

    deploy: d50c46f0360fd6c905b47ad48885df2367ef0ceb

---
+++ precache-manifest.f158ac4e609f4bb57e21d0417324aab6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "112a6921f17adb6c2b6d78a451123960",
+    "url": "/index.html"
+  },
+  {
+    "revision": "56449b63928ab934d6c2",
+    "url": "/static/js/2.9fc44c36.chunk.js"
+  },
+  {
+    "revision": "d0d8ce50dad9db657be2e269623ccb09",
+    "url": "/static/js/2.9fc44c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "47ced76f9d2fd92eb406",
+    "url": "/static/js/main.da820173.chunk.js"
+  },
+  {
+    "revision": "58e95fa7832e2f8e65a4",
+    "url": "/static/js/runtime-main.aedb1fe1.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/doodle/precache-manifest.79d4592b2778133520a06ef5a82855aa.js"
+  "/precache-manifest.f158ac4e609f4bb57e21d0417324aab6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/doodle/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
