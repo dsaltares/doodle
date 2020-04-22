89411263b509ad568f14d88d54a3b1ec1acae479
David Saltares <david.saltares@gmail.com>
2020-04-22 09:32:37 +0300

    back to always localhost for now

--- web-app/src/store/endpoints.ts
+++ web-app/src/store/endpoints.ts
@@ -1,3 +1,3 @@
 export const LIVE_SERVER_SOCKET = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3001'
-  : 'ws://ec2-3-250-18-16.eu-west-1.compute.amazonaws.com:3000';
+  : 'http://localhost:3001';
