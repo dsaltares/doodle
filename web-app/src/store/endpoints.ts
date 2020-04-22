0062b2becd234a72ce866bd6dcb8c136c3c5eb50
David Saltares <david.saltares@gmail.com>
2020-04-22 09:15:21 +0300

    try ws://

--- web-app/src/store/endpoints.ts
+++ web-app/src/store/endpoints.ts
@@ -1,3 +1,3 @@
 export const LIVE_SERVER_SOCKET = process.env.NODE_ENV === 'development'
   ? 'http://localhost:3001'
-  : 'ec2-3-250-18-16.eu-west-1.compute.amazonaws.com:3000';
+  : 'ws://ec2-3-250-18-16.eu-west-1.compute.amazonaws.com:3000';
