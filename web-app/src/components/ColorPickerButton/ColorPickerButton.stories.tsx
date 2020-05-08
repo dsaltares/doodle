134fc5fb2f436f5fbacd1c0b6034f79e493a5967
David Saltares <david.saltares@gmail.com>
2020-05-08 19:37:12 +0300

    add black to color palette

---
+++ web-app/src/components/ColorPickerButton/ColorPickerButton.stories.tsx
@@ -0,0 +1,15 @@
+import React, { useState } from 'react';
+
+import ColorPickerButton from './ColorPickerButton';
+
+export default { title: 'ColorPickerButton' }
+
+export const Picker = () => {
+  const [color, setColor] = useState<string | undefined>(undefined);
+  return (
+    <ColorPickerButton
+      color={color}
+      onColorChange={setColor}
+    />
+  );
+};
--- web-app/src/components/ColorPickerButton/ColorPickerButton.tsx
+++ web-app/src/components/ColorPickerButton/ColorPickerButton.tsx
@@ -4,24 +4,26 @@ import { CirclePicker, ColorResult } from 'react-color';
 import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
 
 import ExpandableToolBarButton from '../ExpandableToolBarButton';
+import Colors from './Colors';
 
 type Props = {
-  color: string,
+  color?: string,
   onColorChange: (color: string) => void,
 }
 
 const ColorPickerButton: FunctionComponent<Props> = ({
-  color,
+  color=Colors[0],
   onColorChange,
 }) => {
   const handleOnChangeComplete = (color: ColorResult) => onColorChange(color.hex);
 
   return (
     <ExpandableToolBarButton
-      title="Color"
+      title='Color'
       icon={<FiberManualRecordIcon style={{ color }}/>}
       renderContent={({ close }) => (
         <CirclePicker
+          colors={Colors}
           color={color}
           onChangeComplete={(color) => {
             close();
